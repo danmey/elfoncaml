0f3ac262752c56e174a7b6b3679cd2d33246ca33
Wojciech Meyer <wojciech.meyer@example.com>
2011-07-22 02:11:25 +0100

    Prog5.ml is not crashing anymore.

--- examples/prog5.ml
+++ examples/prog5.ml
@@ -43,39 +43,42 @@ let _ =
                         sh_entsize = 0l;
                       };
                       
-                  (*     let Some scn = newscn elf in *)
-                  (*     let data = SectionData.create scn in *)
-                  (* (\* let c = int_of_char in *\) *)
-                  (*     SectionData.update { data with *)
-                  (*       d_align = 1L; *)
-                  (*   (\* d_buf = Some (Bigarray.Array1.of_array Bigarray.int8_unsigned Bigarray.c_layout *\) *)
-                  (*   (\*                 (Array.map c [| *\) *)
-                  (*   (\*                   char_of_int 0; *\) *)
-                  (*   (\*                   '.'; 'f'; 'o'; 'o'; char_of_int 0; *\) *)
-                  (*   (\*                   '.'; 's'; 'h'; 's';'t'; char_of_int 0; *\) *)
-                  (*   (\*                   'r'; 't'; 'a'; 'b'; char_of_int 0; *\) *)
-                  (*   (\*                 |])); *\) *)
-                  (*       d_off = 0L; *)
-                  (*       d_size = 17L; *)
-                  (*       d_type = T_BYTE; *)
-                  (*       d_version = EV_CURRENT; *)
-                  (*     }; *)
-                  (*     SectionHeader.update { (SectionHeader.from_section scn) with *)
-                  (*       sh_name = 6l; *)
-                  (*       sh_type = SHT_STRTAB; *)
-                  (*   (\* sh_flags = [SHF_STRINGS;SHF_ALLOC]; *\) *)
-                  (*       sh_entsize = 0l; *)
-                  (*     }; *)
-                  (*     (\* update_shstrndx elf (ndxscn scn); *\) *)
-                  (*     ignore (update elf C_NULL); *)
-                  (*     let ehdr = Elf32Header.get ehdr.ehdr in *)
-                  (*     ProgramHeader.update { phdr with *)
-                  (*       p_type = PT_PHDR; *)
-                  (*       p_offset = ehdr.e_phoff; *)
-                  (*       p_filesz = fsize T_PHDR 1l EV_CURRENT; *)
-                  (*     }; *)
-                  (*     flagphdr elf C_SET F_DIRTY; *)
-                  (*     ignore (update elf C_WRITE); *)
+                  match newscn elf with
+                    | None -> failwith "newscn"
+                    | Some scn ->
+                      let data = Elf_Data.create (newdata scn) in
+                      let c = int_of_char in
+                      Elf_Data.update { data with
+                        d_align = 1L;
+                    (* d_buf = Some (Bigarray.Array1.of_array Bigarray.int8_unsigned Bigarray.c_layout *)
+                    (*                 (Array.map c [| *)
+                    (*                   char_of_int 0; *)
+                    (*                   '.'; 'f'; 'o'; 'o'; char_of_int 0; *)
+                    (*                   '.'; 's'; 'h'; 's';'t'; char_of_int 0; *)
+                    (*                   'r'; 't'; 'a'; 'b'; char_of_int 0; *)
+                    (*                 |])); *)
+                        d_off = 0L;
+                        d_size = 17L;
+                        d_type = T_BYTE;
+                        d_version = EV_CURRENT;
+                      };
+                      let Some shdr = getshdr scn in
+                      let shdr = Elf32_Shdr.create shdr in
+                      Elf32_Shdr.update { shdr with
+                        sh_name = 6l;
+                        sh_type = SHT_STRTAB;
+                        (* sh_flags = [SHF_STRINGS;SHF_ALLOC]; *)
+                        sh_entsize = 0l;
+                      };
+                      (* update_shstrndx elf (ndxscn scn); *)
+                      ignore (update elf C_NULL);
+                      Elf32_Phdr.update { phdr with
+                        p_type = PT_PHDR;
+                        p_offset = ehdr.e_phoff;
+                        p_filesz = fsize T_PHDR 1l EV_CURRENT;
+                      };
+                      flagphdr elf C_SET F_DIRTY;
+                      ignore (update elf C_WRITE);
                       ends elf;
                       Unix.close fd;
                       exit 0
