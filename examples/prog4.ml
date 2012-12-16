28c4bce996fa2bfff286dd82bdc4849ee6e9cb75
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-12-16 12:16:35 +0000

    More fixes

--- examples/prog4.ml
+++ examples/prog4.ml
@@ -11,27 +11,29 @@ let _ =
   else
     match Elf.version `CURRENT with
       | `NONE -> err "elf_version() failed: %s."
-      | _ -> 
-        begin 
+      | _ ->
+        begin
           let fd = Unix.openfile Sys.argv.(1) [Unix.O_RDONLY] 0 in
           let elf = begins fd C_READ None in
           let kind = kind elf in
           match kind with
             | Elf.K_ELF -> begin
               let shstrndx = getshdrstrndx elf in
-              let sections = sections elf in 
-              List.iter 
-                (fun sec -> 
+              let sections = sections elf in
+              List.iter
+                (fun sec ->
                   let shdr = getshdr sec in
                   let shdr = Shdr.create shdr in
                   let name = strptr elf shstrndx (Int64.to_int shdr.Shdr.sh_name) in
-                  let Some data = getdata sec in
-                  let data = Data.create data in
-                  Array.iter print_int data.Data.d_buf;
-                  Printf.printf "Section %-4.4d %s\n"  (ndxscn sec) name) sections
+                  match getdata sec with
+                  | Some data ->
+                      let data = Data.create data in
+                      Array.iter print_int data.Data.d_buf;
+                      Printf.printf "Section %-4.4d %s\n"  (ndxscn sec) name
+                  | None -> err "getdata failed: %s"
+                ) sections
+
+
             end
             | _ -> print_endline "Uknown elf kind"
         end
-
-
-            
